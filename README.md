2f651932c4b561a6527761cfa9f805ef604b5225
Calvincs <calvincs@gmail.com>
2021-03-16 21:38:17 -0500

    pushing updates from dev branch

--- README.md
+++ README.md
@@ -2,12 +2,117 @@
  - Proof Of Concept NFT token builder for the XRP ledger
  - Not production ready, only an example for demonstration
  - XRP Ledger Testnet only
+ - Please feel free to fork this, alter this, go build amazing things :-)
 
 
-## Use
+## Build & Run
 
 `npm install`
 
 `node index.js`
 
-## Only Proof of Concept
+ - Ensure you've configured the setup.yml
+ - Follow along with the prompts
+ - You can use Xumm w/ the QR codes to help add and send funds to your TEST wallets Ref Xumm for setup of Testnet
+ - All interactions with the script, including QR codes are logged to the creation.log file
+
+### nft-content folder
+  Place all the content you want to include in your NFT in this directory.  
+  All the contents will be packaged up and deployed over IPFS for you. 
+  The resulting CIDv1 hash will be added to the XRP wallet you designate it to as well.
+
+  - I removed the BigBuckBunny.mp4 from the GitRepo due to size, you can download it here: https://ipfs.io/ipfs/bafybeiccp7ikixrum3kizkbezqtvl4fylomiihdtg4pm2wyhojhsp3wdgu/BigBuckBunny.mp4
+
+
+### template folder
+  The script attempts to make a index.html file which is placed in the nft-content folder for you during build.
+  This index.html file is generated base on the setup.yml configuration.
+  You can have multiple templates in the directory, and update the setup.yml to use the template which best displays your content.
+  It is recommended your index.html file be as generic as possible, as adding to many libraries to your work may not work well in the future.
+  As an example, browsers which support jQuery x.0.0 may not support jQuery x.0.0 five + years from now.  
+  The template engine uses nunjucks, and gets its feed from the `config` variable used in the script.
+  You should preview the index.html file before publishing, the script allows you to do this, its a built in step.
+
+  - This is also why the generated meta.json file is important. Any future display tool used to view the NFT can use this data as well to display the work.
+  - If you develope an amazin template to show off your NFT, please let us see it! Share on twitter and please tag me, would love to see it!
+
+### setup.yml
+  The setup.yml file is well documented with comments
+  Please validate all the settings in the setup.yml file before generating your content
+  The script will ask you multiple times to validate your given settings
+    
+ - Leaving some fields blank will result in the script asking you for said data, but best practice would be to pre-define said values
+
+
+### meta.json
+  The meta.json file contains details linking the IPFS file contents to the NFT wallet and the creators/artists wallet.
+  This is generated during the build process, and is re-built during each run.
+  Example output:
+  `json
+        {
+            "author": {
+                "wallet": "X7ART2fww9nxR1xWxM9WjQzY3j3C7eVaquLorx4aMio6UL8",
+                "name": "Calvin Schultz",
+                "email": "calvin@ventibean.com",
+                "twitter": "@calcs9 \n@CloudXmpl",
+                "website": "http://xmpl.cloud/",
+                "bio": "Autodidact, driven by possibilities, passionate about Technology, Security, Personal Finance, Blockchain, Real Estate, Science\nDev: Python,JS,Go,Rust,Java,etc\nFind me on Twitter @calcs9 or @CloudXmpl"
+            },
+            "details": {
+                "title": "Big Buck Bunny",
+                "description": "When Big Bucks day gets turned upside down by the loss of his favorite butterflies via some rotten rodents, he takes to the offensive to avenge his friends. This is a short animated, comedic, and light-hearted movie that has stood the test of time.\nCode-named \"Project Peach\" by the Blender Institute, the film was made using a free and open-source software application called Blender.\n\nThis short film was released in 2008 under the Creative Commons Attribution 3.0 license.\n\nThis NFT is a distribution of that original work.  By releasing this work as an NFT, It's my hope to accomplish the following:\n1) Preserve the work of this media on a decentralized platform\n2) Bring awareness to XRPs ability to create NFTs on the ledger easily and efficiently\n3) Show the benefits of having an XRP wallet as the NFT vs a \"token\"\n4) Raise money for some great open source projects/foundations\n\nClick the above floating BigBuckBunny to watch short film if you have an IPFS enabled browser.",
+                "cover": "bunny.png",
+                "link": "BigBuckBunny.mp4",
+                "legal": "The works contained in this package are protected by U.S. and International copyright laws\nCreative Commons Attribution 3.0 license\n(c) copyright 2008, Blender Foundation / www.bigbuckbunny.org",
+                "NFTWalletAddress": "rNtm6FCQDBBjRTcdTeBFXoDr4BPSgpE34M",
+                "NFTWalletXAddress": "XVNFT2byUWwpzimqh1LjN2RcrYBNbymJEChnuSrE1n4n8jH"
+            },
+            "hashes": [
+                {
+                    "file": "/nft-content/BigBuckBunny.mp4",
+                    "cid": "QmNT6isqrhH6LZWg8NeXQYTD9wPjJo2BHHzyezpf9BdHbD",
+                    "sha256": "20b80994904d40acbf6224024097a2ce5a1ea7130478e57162a38af1b876dfce"
+                },
+                {
+                    "file": "/nft-content/bunny.png",
+                    "cid": "QmRe6bYHv4XtsA8oXJeNSY6NqmDYa9Q3JBhUtZCEAC6yDQ",
+                    "sha256": "7ef51475fe83169530a16403a9941afde156fc87aa058a8500131a23de600f9a"
+                },
+                {
+                    "file": "/nft-content/butterfly.gif",
+                    "cid": "Qma7AZJGSeDPezaK2qqej4EiYuptSTjvnAziriwaauAXf7",
+                    "sha256": "110b30dfa67ae6668a335240b12362a42aeceafb374bf8617816e8a13ff12ffc"
+                }
+            ],
+            "created": 1615926994199,
+            "framework": "https://github.com/calvincs/xrpl-nft-creator"
+        }
+
+  `
+
+#### Why use an XRP wallet?
+ Here are a few reasons I believe XRP wallets are a great choice for NFTs
+
+ - Multiple people can control/own a single asset via MultiSign
+ - Wallets can be easily transferred between users by updating the Regular Signers Key
+ - Adding a pointer from the XRP wallet to a decentralized data store aka IPFS is straight forward
+ - XRP + IPFS allows an easily traceable path of ownership of an NTF and the original creator/artist
+ - While a Wallet does require a 20 XRP funding minimum, future TXs are relatively inexpensive as compared to some other solutions
+ - Having access to the Wallet allows for exciting features that are difficult to implement in a Token alone.
+    - Some examples:
+        Wallets can sign data
+        Wallets can receive and send payments
+        Wallets using Xumm can authenticate to other services ;-)
+        Hooks?
+        Xmpl.Cloud?
+
+
+#### Example living on the XRP Ledger MainNet
+ - NFT Address: XVNFT2byUWwpzimqh1LjN2RcrYBNbymJEChnuSrE1n4n8jH
+ - IPFS ipfs://bafybeiccp7ikixrum3kizkbezqtvl4fylomiihdtg4pm2wyhojhsp3wdgu/meta.json
+
+
+ ### Contact
+ Twitter calcs9
+
+ # Enjoy
--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const path = require('path')
 const Yaml = require('js-yaml');
 const logs = require('./log');
 const prompt = require('prompt-sync')({sigint: true});
-const { hashElement } = require('folder-hash');
+const crypto = require('crypto');
 const RippleAPI = require('ripple-lib').RippleAPI;
-const winston = require('winston/lib/winston/config');
 const { create, globSource } = require('ipfs-core');
+const nunjucks = require('nunjucks');
+const open = require('open');
+const xAddr = require('xrpl-tagged-address-codec')
+const Hash = require('ipfs-only-hash')
+const qrcode = require('qrcode-terminal');
 
 
 // Pull in configuration options - setup.yml
@@ -21,11 +25,10 @@ try {
   process.exit(-1);
 }
 
-
 // Global Things
 const xAPI = new RippleAPI({ server: config.xrp.network });
 let iAPI;
-
+qrcode.setErrorLevel('H');
 
 // catch and display the errors nicely
 function CatchError(err) {
@@ -40,8 +43,28 @@ function CatchError(err) {
     } else {
       logs.error('error in CatchError:: argument is not an object');
     }
+    prompt('something went wrong, press Enter to exit script...')
+    process.exit()
   }
 
+// clean the working nft-content directory from previous runs
+async function CleanNftDirectory() {
+  try {
+    logs.info(`clearing any old generated files from ${config.settings.contentPath}`);
+    let metaFile = `${config.settings.contentPath}${path.sep}meta.json`;
+    let htmlFile = `${config.settings.contentPath}${path.sep}index.html`;
+    // clean out any file that maybe left over from previous runs (index.html | meta.json)
+    const cleanFiles = [metaFile,htmlFile];
+    await Promise.all(cleanFiles.map(async (file) => {
+          logs.info(`found old file ${file}, attempting removal`)
+          fileSys.unlinkSync(file);
+    }));
+  } catch (error) {
+    if (!error.code === "ENOENT") {
+      CatchError(error)
+    }
+  }
+}
 
 // write JSON to System State file
 async function WriteMetaData(data) {
@@ -54,15 +77,13 @@ async function WriteMetaData(data) {
     }
   }
 
-
 // write JSON to System State file
 async function ValidateFunding(walletAddress) {
     try {
         return new Promise((resolve, reject) => {
             let walletFundedChecking = setInterval(async () => {
                 try {
-                    let testAddr = "rfHmFePnp6P4sQV2g9L8cC6CZv1UNLScqy"
-                    let resp = await xAPI.getSettings(walletAddress);
+                    await xAPI.getSettings(walletAddress);
                     logs.info(`validating ${walletAddress} is funded...`)
                     clearInterval(walletFundedChecking);
                     resolve()
@@ -80,19 +101,20 @@ async function ValidateFunding(walletAddress) {
                         default:
                             logs.warn(`encountered error during validation of wallet funding: ${JSON.stringify(error, null, 2)}`)
                             reject()
+                            break
                     }
                 }
-            }, 5000);
+            }, 10000); //Will check every 10 seconds
         });
     } catch (error) {
       CatchError(error)
     }
   }
 
-
 //Push nft-content files, returns root cid
 async function PushNFTFiles() {
     try {
+        logs.info(`if your pushing a large NFT over a slow connection, this may take some time...`)
         //options specific to globSource
         const globSourceOptions = { recursive: true };
         
@@ -101,7 +123,7 @@ async function PushNFTFiles() {
             pin: true,
             wrapWithDirectory: true,
             cidVersion: 1,
-            timeout: 10000
+            timeout: 300000
         };
         let rootCID = "";
         for await (const file of iAPI.addAll(globSource('nft-content', globSourceOptions), addOptions)) {
@@ -116,6 +138,49 @@ async function PushNFTFiles() {
     }
 }
 
+//Fetch nft-content files cids, returns cids for nft-content files
+//Also gathers SHA256 for each file
+async function GetNFTFileHashes() {
+  try {
+      logs.info(`gathering IPFS CID & SHA256 hashes to add to meta data file...`)
+      //options specific to globSource
+      const globSourceOptions = { recursive: true };
+      
+      let cids = [];
+      for await (let value of globSource('nft-content', globSourceOptions)) {
+        if (value.path != "/nft-content") {
+          let fileData = await fileSys.readFileSync(value.content.path);
+          const cid = await Hash.of(fileData);
+          console.log(JSON.stringify(cid))
+          const hash = crypto.createHash('sha256');
+          hash.update(fileData);
+          const hdigest = hash.digest('hex');
+          cids.push({'file':value.path, 'cid': cid, 'sha256': hdigest})
+          logs.info(`adding '${value.path}' with cid hash '${hdigest}' to meta data...`)
+        }
+      }
+      return cids
+  } catch (error) {
+      CatchError(error)
+  }
+}
+
+//Generate and write index.html file to nft-content folder
+async function WriteIndexHtml() {
+  try {
+      logs.info(`generating index.html file...`)
+      nunjucks.configure({ autoescape: config.settings.tempateHtmlEscape });
+      let templatePath = config.settings.templatePath + path.sep + "index.html"
+      console.log(templatePath)
+      let contentPath = config.settings.contentPath + path.sep + "index.html"
+      let htmlPage = await nunjucks.render(templatePath, config);
+      fileSys.writeFileSync(contentPath, htmlPage);
+      logs.info(`index.html file generated: ${contentPath}`)
+      return contentPath
+  } catch (error) {
+      CatchError(error)
+  }
+}
 
 //Update xrp wallet data
 async function updateXRPWalletData(walletData, walletAddress) {
@@ -157,95 +222,203 @@ async function updateXRPWalletData(walletData, walletAddress) {
     }
 }
 
-
+// - Main execution block for script
 async function main() {
     try {
         // Init IPFS
         iAPI = await create({ repo: config.ipfs.repopath });
+        // Clean nft-content folder before run
+        await CleanNftDirectory()
 
+        // Statements to end user on usage of script
         logs.warn("DO NOT USE THIS IN PRODUCTION, THIS IS A Proof Of Concept")
         logs.warn("Use the XRP TestNet for testing\n\n")
+        logs.warn("You can get a XRP Test account here: https://xrpl.org/xrp-testnet-faucet.html")
+        prompt("By pressing enter, you agree and understand this is a Proof of Concept, and SHOULD NOT be used for production for any reason!")
+        logs.info(`Current setup.yml values:\n${JSON.stringify(config.settings.meta, null, 4)}\n`)
+        prompt("Please ensure you filled in the correct details for this NFT in the setup.yml file\nPress Return to continue or Ctrl+C if you need to exit")
 
-        // Ask questions about the contents, meta data
-        config.meta.name = prompt("What is the name of this NF Token: ")
-        config.meta.description = prompt("Description of the work contained in the NF Token: ")
+        // Ask questions about the contents, meta data, etc if blank
+        if (!config.settings.meta.details.title) {
+          config.settings.meta.details.title = prompt("What is the title of this NF Token: ")
+        }
+        if (!config.settings.meta.details.description) {
+          config.settings.meta.details.description = prompt("Description of the work contained in the NF Token: ")
+        }
+        if (!config.settings.meta.details.cover) {
+          config.settings.meta.details.cover = prompt("Name of the cover photo you want to use, should be in the nft-content folder\nExample: cover.png")
+        }
+        if (!config.settings.meta.details.legal) {
+          config.settings.meta.details.legal = prompt("Provide any legal usage rights you want to provide with this NFT.")
+        }
 
-        //Get additional meta of nft-content folder contents
-        config.meta.contents = await hashElement(config.settings.contentPath, {algo: "sha256", encoding: "hex"})
-        
-        // Generate a new XRP wallet, give address to the user for funding ( MUST BE PRE-FUNDED w/ 21 xrp )
+        // Get additional meta of nft-content folder contents
+        // - Content IDentifiers for IPFS and SHA256 values for files
+        config.settings.meta.hashes = await GetNFTFileHashes()
+
+        // Setup a new XRP wallet ( MUST BE PRE-FUNDED w/ 21 xrp )
         // Starts the XRP Ledger API interface
         logs.info(`Init connection to xrp ledger: ${JSON.stringify(config.xrp.network, null, 4)}`);
-        let output = await xAPI.connect();
+        await xAPI.connect();
 
         // Generate NFT wallet address to be funded by author address
-        const address = xAPI.generateAddress();
-        logs.info(`NFT Wallet Address: ${address.address}`);
-        logs.info(`NFT Wallet Secret: ${address.secret}`);
+        // - (options) Use existing address seed OR create a new wallet address
+        let address = {};
+        let secret;
+        let validAcc = false;
+        do {
+          let resp = prompt("Would you like to supply your own account seed? Ex: snfFLJ95fJr3e.... [ y/n ] ")
+          switch(resp){
+            case "y":
+              secret = prompt("please enter your secret: ")
+              if (await xAPI.isValidSecret(secret)) {
+                let tempAddr = await xAPI.deriveKeypair(secret);
+                address.address = await xAPI.deriveAddress(tempAddr.publicKey)
+                address.xAddress = await xAddr.Encode({ account: address.address})
+                address.secret = secret
+                logs.info(`account secret appears valid: '${secret}'`)
+                validAcc = true;
+                break
+              } else {
+                logs.warn(`the provided secret was not valid, please try again: '${secret}'`)
+                break
+              }
+            case "n":
+                logs.info(`generating a new wallet address...`)
+                address = await xAPI.generateAddress();
+                validAcc = true;
+                break
+            default:
+              logs.warn(`invalid option given: '${resp}' use Ctrl+C to exit if required, otherwise try again...`)
+              break
+          }
+        } while(!validAcc);
 
+        // Display the account for the user
+        logs.info(`NFT Wallet Address: (Classic)  '${address.address}'`)
+        qrcode.generate(address.address, {small: true}, function(qrcode){
+          logs.warn(`Classic Address QR Code\n${qrcode}\n\n\n`)
+        });
+        logs.info(`NFT Wallet Address: (XAddress) '${address.xAddress}'`)
+        qrcode.generate(address.xAddress, {small: true}, function(qrcode){
+          logs.warn(`X Address QR Code\n${qrcode}\n\n\n`)
+        });
+        logs.warn(`NFT Wallet Secret:  ${address.secret}  <-- This is logged, so please take care to remove it after`);
+        qrcode.generate(address.address, {small: true}, function(qrcode){
+          logs.warn(`Address Secret QR Code\n${qrcode}\n\n\n`)
+        });
         // Add the public wallet address to the meta data
-        config.meta.NFTWalletAddress = address.address.toString()
+        config.settings.meta.details.NFTWalletAddress = address.address.toString()
+        config.settings.meta.details.NFTWalletXAddress = address.xAddress.toString()
+
+        // - Write Meta data to nft-content folder for IPFS packaging
+        config.settings.meta.created = new Date().getTime();
+        config.settings.meta.framework = "https://github.com/calvincs/xrpl-nft-creator";
+        await WriteMetaData(config.settings.meta);
+
+        // Render the index.html file, found in the template folder.
+        // - customize the index.html file for each of your NFTs if you like, otherwise use a default template setup
+        let indexFilePath = await WriteIndexHtml();
+        let preview;
+        do {
+          let resp = prompt("Would you like to preview the NFT index.html page [ y/n ] ")
+          switch(resp){
+            case "y":
+              // attempts to launch users browser, best if user has Brave Browser
+              await open(indexFilePath)
+              preview = true
+              break
+            case "n":
+                preview = true
+                break
+            default:
+              logs.warn(`invalid option given: '${resp}' use Ctrl+C to exit if required, otherwise try again...`)
+              break
+          }
+        } while (!preview);
+
+        // - Before they fund, validate the data
+        logs.warn(`PLEASE VALIDATE DATA BEFORE FUNDING`)
+        logs.warn(`\n${JSON.stringify(config.settings.meta, null, 2)}\n`)
+        let validation = false;
+        do {
+          let resp = prompt("Does the information look correct? [ y/n ] ")
+          switch(resp){
+            case "y":
+              logs.info('User validated information, ready for NFT funding...')
+              validation = true;
+              break
+            case "n":
+                logs.warn(`User stated information is incorrect, exiting script`)
+                process.exit(0)
+                break // - wont be used, but you know, eslint things.. 
+            default:
+              logs.warn(`invalid option given: '${resp}' use Ctrl+C to exit if required, otherwise try again...`)
+              break
+          }
+        } while(!validation)
 
         // You MUST FUND THE NEW ADDRESS NOW for this to work
-        logs.warn(`YOU MUST FUND THE NFT ADDRESS NOW (CLASSIC): ${address.address.toString()}`)
-        logs.warn(`YOU MUST FUND THE NFT ADDRESS NOW (XADDRESS): ${address.xAddress.toString()}`)
+        logs.warn(`YOU MUST FUND THE NFT ADDRESS w/ 21 XRP (CLASSIC): '${address.address.toString()}' (XADDRESS): '${address.xAddress.toString()}'`)
 
         // Validate the above address is funded...
-        logs.info(`Waiting for wallet funding to complete...`)
+        logs.info(`Waiting for wallet funding to complete, will check every 10 seconds till validated...`)
         await ValidateFunding(address.address)
         logs.warn(`validation completed, wallet ${address.address} is funded`)
 
-        //Now that the wallet is funded, lets bundle up the files in IPFS
-        // - Write Meta data to nft-content folder for IPFS wrap up
-        config.meta.framework = "https://github.com/calvincs/xrpl-nft-creator";
-        await WriteMetaData(config.meta);
-
+        // Now that the wallet is funded, lets bundle up the files in IPFS
         // - Push nft-content files to IPFS w/ PIN, get CID value
         let online = await iAPI.isOnline();
         logs.info(`IPFS Online status: ${online}`);
 
         let rootCID = await PushNFTFiles();
         let rootString = rootCID.cid.toString();
-        logs.info(`nft-content root string recorded as: ${rootString}`);
-
+        let rootStringURL = `ipfs://${rootString}/`;
+        logs.info(`nft-content root string recorded as: ${rootStringURL}`);
+        
         //Get string ready for XRPL Wallet Domain Field
-        let xrpDomainField = new Buffer.from(rootString).toString('hex').toUpperCase();
-        logs.info(`Attempting to set ${address.address} Domain field to IPFS address`)
+        let xrpDomainField = new Buffer.from(rootStringURL).toString('hex').toUpperCase();
+        logs.info(`Attempting to set ${address.address} Domain field to IPFS address of ${rootStringURL}`)
 
         // Write the data to the NFT Wallet
         await updateXRPWalletData({"Domain": xrpDomainField}, address)
 
         // You should pin this remotely, display some messages for user
+        logs.info("Content is best viewed with an IPFS enabled browser, like Brave Browser: https://brave.com/")
+        logs.info(`Validate your NFT data is live: ${rootStringURL}`)
         logs.info(`Validate your NFT data is live: https://gateway.ipfs.io/ipfs/${rootString} `)
-        logs.info(`Validate your NFT data is live: https://cloudflare-ipfs.com/ipfs/${rootString} `)
+        logs.info(`Validate your NFT data is live: https://ipfs.io/ipfs/${rootString} `)
         logs.warn('Your data is being served from this temp IPFS node, you should PIN this NFT externally for long term distribution')
-        logs.info("...")
-        logs.warn('Pinning Service: https://pinata.cloud/  (no affiliation), or run your own IPFS node: https://ipfs.io/')
-        logs.info(`See your testnet NFT wallet here: https://testnet.xrpl.org/accounts/${address.address}`)
+        logs.warn('Pinning Service: https://pinata.cloud/ Or run your own IPFS node: https://ipfs.io/')
+        logs.info(`See your Testnet NFT wallet here: https://testnet.xrpl.org/accounts/${address.address}`)
+        logs.warn(`Remember this activity was all logged to xrpl-nft-creator.log, and contains your secrets during this creation session!`)
         logs.info("...")
 
-        logs.warn('This node application will end in 10 minutes, and stop the IPFS node and close the XRP ledger connection')
+        logs.warn('This node application will end in 10 minutes. This will stop the IPFS node and close the XRP ledger connection')
         logs.warn('Use Ctrl+C to end the program sooner, this will halt hosting of your local NFT token data')
+        logs.info('Will show IPFS swarm data every 30 seconds till script ends')
 
-
-        
-        // Ideally the Author wallet will fund the NFT wallet to show a connection to the author as well
-        // Generate IPFS data containting the NFT data
-        // Push the data to the wallet, populate the wallet domain field w/ IPFS id of content.
-
-        //User can now publish the IPFS content if they choose, as well, can give the wallet to another person for trade.
-        //The joining of the XRPL wallet address with the hash of the IPFS contents establishes ownership and control
-        //The ability of the author to link an origin account with the new wallet also establishes authoriship
-        //The author can publish an entire collection and have the original attribution to themselves.
-        //The owner can also delete the wallet, giving the NFT public ownership vs private ownership. 
-
-        // I wrote this in 45 minutes.. so hold judgment :-P  Enjoy...
+        // Show number of swarm peers for IPFS
+        let peerHealthCheck = setInterval(async () => {
+          try {
+              if (iAPI.isOnline() == false) {
+                  clearInterval(peerHealthCheck);
+                  logs.info('ipfs node is no longer online, stopping peer health check')
+              } else {
+                  const peers = await iAPI.swarm.peers()
+                  logs.debug(`this IPFS node now has ${peers.length} swarm peers...`)
+              }
+          } catch (error) {
+              CatchError('An error occurred trying to check our peer counts:', error)
+          }
+      }, 30000); //Every 30 seconds, check swarm count
 
         //Final Disconnect after wait... 
         setTimeout(async function(){
             logs.info('Time to close things down...')
-            xAPI.disconnect()
-            iAPI.stop();
+            await xAPI.disconnect()
+            await iAPI.stop();
+            process.exit()
         }, 600000);
 
     } catch (error) {
