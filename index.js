e5db8a8c668e5ffbbd49bd86111f50e67d90ade2
Calvincs <calvincs@gmail.com>
2021-05-26 21:18:59 -0500

    tmp patch due to ipfs updates

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ const pinataSDK = require('@pinata/sdk');
 const CID = require('cids')
 
 
+// Patching for IPFS issues, use p2p bootstrap
+const Libp2p = require('libp2p')
+const TCP = require('libp2p-tcp')
+const MulticastDNS = require('libp2p-mdns')
+const Bootstrap = require('libp2p-bootstrap')
+const KadDHT = require('libp2p-kad-dht')
+const MPLEX = require('libp2p-mplex')
+const { NOISE } = require('libp2p-noise')
+
 // Pull in configuration options - setup.yml
 let config;
 try {
@@ -597,6 +606,89 @@ async function BuildDomainPointer(resources) {
   }
 }
 
+const libp2pBundle = (opts) => {
+  // Set convenience variables to clearly showcase some of the useful things that are available
+  const peerId = opts.peerId
+  const bootstrapList = opts.config.Bootstrap
+
+  // Build and return our libp2p node
+  // n.b. for full configuration options, see https://github.com/libp2p/js-libp2p/blob/master/doc/CONFIGURATION.md
+  return Libp2p.create({
+    peerId,
+    addresses: {
+      listen: ['/ip4/127.0.0.1/tcp/0']
+    },
+    // Lets limit the connection managers peers and have it check peer health less frequently
+    connectionManager: {
+      minPeers: 25,
+      maxPeers: 100,
+      pollInterval: 5000
+    },
+    modules: {
+      transport: [
+        TCP
+      ],
+      streamMuxer: [
+        MPLEX
+      ],
+      connEncryption: [
+        NOISE
+      ],
+      peerDiscovery: [
+        MulticastDNS,
+        Bootstrap
+      ],
+      dht: KadDHT
+    },
+    config: {
+      peerDiscovery: {
+        autoDial: true, // auto dial to peers we find when we have less peers than `connectionManager.minPeers`
+        mdns: {
+          interval: 10000,
+          enabled: true
+        },
+        bootstrap: {
+          interval: 30e3,
+          enabled: true,
+          list: bootstrapList
+        }
+      },
+      // Turn on relay with hop active so we can connect to more peers
+      relay: {
+        enabled: true,
+        hop: {
+          enabled: true,
+          active: true
+        }
+      },
+      dht: {
+        enabled: true,
+        kBucketSize: 20,
+        randomWalk: {
+          enabled: true,
+          interval: 10e3, // This is set low intentionally, so more peers are discovered quickly. Higher intervals are recommended
+          timeout: 2e3 // End the query quickly since we're running so frequently
+        }
+      },
+      pubsub: {
+        enabled: true
+      }
+    },
+    metrics: {
+      enabled: true,
+      computeThrottleMaxQueueSize: 1000,  // How many messages a stat will queue before processing
+      computeThrottleTimeout: 2000,       // Time in milliseconds a stat will wait, after the last item was added, before processing
+      movingAverageIntervals: [           // The moving averages that will be computed
+        60 * 1000, // 1 minute
+        5 * 60 * 1000, // 5 minutes
+        15 * 60 * 1000 // 15 minutes
+      ],
+      maxOldPeersRetention: 50            // How many disconnected peers we will retain stats for
+    }
+  })
+}
+
+
 //Main execution block for script
 async function main() {
     try {
@@ -612,9 +704,12 @@ async function main() {
 
         // Init IPFS
         logs.info('starting IPFS node for content distribution')
+
+        // - Testing, seems updated IPFS package has some bugs
         iAPI = await create({ 
-          repo: config.ipfs.repopath
-        });
+            repo: config.ipfs.node,
+            libp2p: libp2pBundle
+          });
 
         // Statements to end user on usage of script
         logs.warn("DO NOT USE THIS IN PRODUCTION, THIS IS A Proof Of Concept")
